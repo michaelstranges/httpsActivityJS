c7ba98204ddbd1b65754c04ddfba0ff7402fc873
Michael Stranges <strangesm@gmail.com>
2018-03-19 16:29:33 +0000

    Step 4 commit

---
+++ getHTML.js
@@ -0,0 +1,32 @@
+var https = require("https");
+
+function getHTML(options, callback){
+
+  https.get(options, function(response){
+
+    var myBuffer = "";
+
+    response.setEncoding("utf8");
+
+    response.on("data", function(data){
+      console.log("Got Chunk:", data.length);
+      myBuffer = myBuffer + data;
+
+      callback(myBuffer);
+
+    })
+  })
+
+
+}
+
+function printHTML(html){
+  console.log(html);
+}
+
+var requestOptions = {
+  host: "sytantris.github.io",
+  path: "/http-examples/step4.html"
+};
+
+getHTML(requestOptions, printHTML)
